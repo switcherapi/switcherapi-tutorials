5d9d9ecb2195f855661d62540bd84b04dd450a55
petruki <31597636+petruki@users.noreply.github.com>
2025-07-06 13:28:21 -0700

    switcher-playground-java: Bump switcher-client@2.3.1, updated code snippets

--- switcher-playground-java/src/main/java/com/github/switcherapi/examples/AppPlaygroundThrottle.java
+++ switcher-playground-java/src/main/java/com/github/switcherapi/examples/AppPlaygroundThrottle.java
@@ -15,8 +15,7 @@ public class AppPlaygroundThrottle {
 
 	public static void main(String[] args) {
 		final Switcher feature = getSwitcher(MY_SWITCHER)
-				.throttle(1000)
-				.build();
+				.throttle(1000);
 		
 		for (int i = 0; i < 20; i++) {
 			feature.isItOn();
